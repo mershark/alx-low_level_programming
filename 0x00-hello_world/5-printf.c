d10adc1909cd2167e0ed23b92c6f645251c7efd0
mershark <mershartakyi@gmail.com>
2022-10-13 17:34:46 -0700

    correction of error in 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <studio.h>
+
 /**
  * main - Entry
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
