24c35c55b2ec142fa4ca1fbe6b95b28a4de66720
mershark <mershartakyi@gmail.com>
2022-10-13 17:50:58 -0700

    correction of error 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n)", sizeof(char));
-	printf("size of a char: %i byte9(s\n)", sizeof(int));
-	printf("size of a char: %i byte(s)\n)", sizeof(long int));
-	printf("size of a char: %i byte(s)\n)", sizeof(long long int));
-	printf("size of a chat: %i byte(s)\n)", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
