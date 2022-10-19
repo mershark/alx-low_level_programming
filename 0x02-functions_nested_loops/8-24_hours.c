8b0765f371e76abf89852a6833f552c4354c46ee
mershark <mershartakyi@gmail.com>
2022-10-18 18:39:47 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,32 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour < 23; hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour / 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
+			if (hour < 10)
+			{
+				_putchar('0');
+				_putchar(hour + '0');
+			}
+			else if (hour >= 10)
+			{
+				_putchar((hour / 10) + '0');
+				_putchar((hour % 10) + '0');
+			}
+			if (minute < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(minute + '0');
+			}
+			else if (minute > 10)
+			{
+				_putchar(':');
+				_putchar((minute / 10) + '0');
+				_putchar((minute % 10) + '0');
+			}
 		}
 	}
 }
