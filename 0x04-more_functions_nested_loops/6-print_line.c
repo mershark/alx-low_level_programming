5db198228cd01414dd3676c8193d9be5d53351e3
mershark <mershartakyi@gmail.com>
2022-10-20 08:18:44 -0700

    task 6 fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,11 +11,11 @@ void print_line(int n)
 	int x;
 
 	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-	for (x = 0; x < n; x++)
-	{
-	_putchar('_');
+		for (x = 0; x < n; x++)
+			_putchar(95);
+		_putchar('\n');
 	}
-	}
-	_putchar('\n');
 }
