11c8e94fe0f64df5681609709bf548110a3c74c5
mershark <mershartakyi@gmail.com>
2022-10-20 09:05:40 -0700

    last adv task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer is to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int d = n;
+
+	if (n < 0)
+	{
+	_putchar('_');
+	d = -d;
+	}
+	if ((d / 10) > 0)
+	{
+	print_number(d / 10);
+	}
+	_putchar((d % 10) + '0')
+}
