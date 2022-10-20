a7eace8bef6785b22386fcfef4fe0eab423b23ea
mershark <mershartakyi@gmail.com>
2022-10-20 03:12:52 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * file - main.h
+ * Author: Genius Villagers
+ * Desc: Header contains protypes for all functions
+ * 	used in 0x03-more_functions_nested_loos directory
+ */
+
+int _putchar(char c);
+
+
+#endif
