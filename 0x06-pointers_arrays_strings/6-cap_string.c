6a52c2c933cbcc512346ead442e2b3e027eb1eec
mershark <mershartakyi@gmail.com>
2022-10-27 16:08:49 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,7 @@
  * Return: Capitalized version of the string
  */
 char *cap_string(char *x)
-{	x = "sami,is; in learn programming africa"
+{
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	int len = 13;
 	int a = 0, i;
@@ -16,8 +16,8 @@ char *cap_string(char *x)
 		i = 0;
 		while (i < len)
 		{
-		if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-			s[a] = s[a] - 32;
+		if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+			x[a] = x[a] - 32;
 		i++;
 		}
 		a++;
