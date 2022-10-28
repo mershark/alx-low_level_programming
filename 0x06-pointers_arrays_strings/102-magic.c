0b6a15c01f3d3aa9cad8945da60429d961ee3bf6
mershark <mershartakyi@gmail.com>
2022-10-27 17:16:46 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - this is for magic
+ * Return: 0
+ */
+
 int main(void)
 {
 	int n;
@@ -11,7 +16,6 @@ int main(void)
 /*
  * write your line of code here...
  * Remember:
- * main - magic
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
