7e070e02b0ff018f985dcb077456f906c4d31a4a
mershark <mershartakyi@gmail.com>
2022-11-02 21:12:41 -0700

    task 7

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,10 @@ int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
 int is_prime_number(int n);
 int check_prime(int, int);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int find_strlen(char *s);                                                               
+int check_palindrome(char *s, int len, int index);                                      
+int is_palindrome(char *s);
 
 #endif
