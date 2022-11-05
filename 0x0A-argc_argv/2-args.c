4c7d27f0a4182612b36b5df073036068eb122a79
mershark <113654948+mershark@users.noreply.github.com>
2022-11-04 21:07:16 -0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - print the name of the program
+ * @argc: count arguemnts
+ * @argv: arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+}
