031f6d3cc522c1628750f4d19484df7b8d4a8f8a
mershark <113654948+mershark@users.noreply.github.com>
2022-11-04 21:14:43 -0300

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,10 @@
+#include <stdio.h>                                                                                                                 
+#include "main.h"                                                                                                                  
+#include <stdlib.h>
+
+/**                                                                                                                                
+ * main - print the name of the program                                                                                            
+ * @argc: count arguemnts                                                                                                          
+ * @argv: arguments                                                                                                                
+ * Return: Always 0                                                                                                                
+ */ 
